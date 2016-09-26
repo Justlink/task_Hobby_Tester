8cd75b4d43a991901b7710fa197fc0f6824e86a7
Alex <alexuv@gmail.com>
2016-09-26 23:58:11 +0300

    new method test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,12 @@ import com.addition.Tester;
 public class Main {
 
     public static void main(String[] args) {
-	  Tester t = new Tester();
-      t.setHobbyFields();
-      t.printHobbyFields();
+	    Hobby h = new Hobby();
+        h.setFields(3,"Hobby's name",34.456f);
+        h.printFields();
+
+        Tester t = new Tester();
+        t.setHobbyFields();
+        t.printHobbyFields();
     }
 }
